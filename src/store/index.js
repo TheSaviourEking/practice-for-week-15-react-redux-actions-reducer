d5bd615d8244b6820d1b71f9e262334ace64f708
TheSaviourEking <saviour.eking@outlook.com>
2024-06-01 02:56:40 +0100

    chore: implement Practice: Action Creator and Reducer Setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,17 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import fruitReducer from './fruitReducer';
+import articleReducer from './articleReducer';
 
 // `combineReducers` combines all the reducer functions into one big reducer
 // function, which is typically called `rootReducer`. This is the most important
 // part of this file. You will add your reducers here to work with your various
 // components.
 const rootReducer = combineReducers({
-  fruitState: fruitReducer
+  // fruitState: fruitReducer,
+  // articleState: articleReducer,
+
+  fruit: fruitReducer,
+  article: articleReducer
 });
 
 // `enhancer` allows you to alter the store and add functionality such as the
@@ -29,7 +34,7 @@ if (process.env.NODE_ENV !== 'production') {
   const logger = require('redux-logger').default;
   const composeEnhancers =
     typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true }) : compose;
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true }) : compose;
   enhancer = composeEnhancers(applyMiddleware(logger));
 }
 
